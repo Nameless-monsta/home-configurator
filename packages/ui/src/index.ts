5e9cd12953014e0e7b3a8fa0e3ef12aa4472e659
Mullar A <modar.gh.ali@gmail.com>
2026-07-11 21:40:15 +0400

    feat(ui): export device panel registry

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,6 +2,7 @@ export * from './configurator-model.js';
 export * from './configurator-types.js';
 export * from './configurator.js';
 export * from './control-library.js';
+export * from './device-panels.js';
 export * from './foundation.js';
 export * from './layout.js';
 export * from './navigation-model.js';
