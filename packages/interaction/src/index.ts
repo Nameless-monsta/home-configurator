1c2bc5089a36b68078dbbc0c68dcbc351b76cf73
Mullar A <modar.gh.ali@gmail.com>
2026-07-11 16:17:48 +0400

    feat(interaction): export interaction engine

---
+++ packages/interaction/src/index.ts
@@ -0,0 +1,7 @@
+export * from './animation-director.js';
+export * from './input-engine.js';
+export * from './interaction-engine.js';
+export * from './navigation-engine.js';
+export * from './selection-engine.js';
+export * from './transition-director.js';
+export * from './types.js';
