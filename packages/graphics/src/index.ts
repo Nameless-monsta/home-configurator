a605177da16a20aba1c6bb70e697060f27d1fef2
Mullar A <modar.gh.ali@gmail.com>
2026-07-12 11:09:46 +0400

    feat: export long task monitor

--- packages/graphics/src/index.ts
+++ packages/graphics/src/index.ts
@@ -6,6 +6,7 @@ export * from './environment-manager.js';
 export * from './graphics-engine.js';
 export * from './lighting.js';
 export * from './lod-manager.js';
+export * from './long-task-monitor.js';
 export * from './performance-budget-monitor.js';
 export * from './post-processing.js';
 export * from './quality-manager.js';
